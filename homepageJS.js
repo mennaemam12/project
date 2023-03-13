978b650a20dc841307180743e80b3b0da7819798
shahd <shahdksalah@gmail.com>
2023-03-13 03:38:07 +0200

    homepage2

---
+++ homepageJS.js
@@ -0,0 +1,21 @@
+function show(id){
+    let signin = document.getElementById(id);
+    signin.style.display='block';
+}
+function hide(id){
+    let x = document.getElementById(id);
+    x.style.display='none';
+}
+//change header on scroll
+let header = document.getElementById("myHeader")
+let logo = document.getElementById("logo");
+window.onscroll = function(){
+    if(window.pageYOffset > 15){
+        header.classList.add('sticky');
+        logo.style.width = "60px";
+    }
+    else{
+        header.classList.remove('sticky');
+        logo.style.width = "100px";
+    }
+}
